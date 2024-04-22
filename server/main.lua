4e9ab553850128b21040f8a65a819e26259227f2
azutake <azusa_takemae@dwango.co.jp>
2024-04-22 16:28:14 +0900

    fix by yuka

--- server/main.lua
+++ server/main.lua
@@ -19,10 +19,17 @@ RegisterNetEvent('qb-recycle:server:getItem', function()
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ChanceItem], 'add')
   end
 
+  local chance2 = math.random(1, 10000)
+  if chance2 < 2 then
+    Player.Functions.AddItem(Config.ChanceItem, 1, false)
+    TriggerClientEvent('inventory:client:ItemBox', src, 'techbook_pistol', 'add')
+  end
+
   local luck = math.random(1, 10)
   if luck == 5 then
     local random = math.random(1, 10)
     Player.Functions.AddItem(Config.LuckyItem, random)
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.LuckyItem], 'add')
   end
+
 end)
